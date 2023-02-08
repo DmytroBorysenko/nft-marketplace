5ffd934a7c3e83612b08495ee6cfa8ef1d4ebb89
dborysenko <dborysenko@gloriumtech.com>
2023-02-08 21:55:44 +0200

    added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HttpClientModule } from '@angular/common/http';
-
+import { AngularFireModule } from '@angular/fire/compat';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, AppRoutingModule, LayoutModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AppRoutingModule,
+    LayoutModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBnKGqjzpjt033MEkkSOPbMf_fwylv3kEM',
+    authDomain: 'nft-marketplace-777.firebaseapp.com',
+    projectId: 'nft-marketplace-777',
+    storageBucket: 'nft-marketplace-777.appspot.com',
+    messagingSenderId: '579464734194',
+    appId: '1:579464734194:web:0d03dd7355914c012e376b',
+    measurementId: 'G-ZS55592HZE',
+  },
+};
